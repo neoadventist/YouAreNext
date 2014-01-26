2b8900fbf4467abe6e480ff57e1c47d328b9c82d
Chase DeLuca <chase@slingr.net>
2014-01-26 05:41:05 -0800

    chases php work

--- client/res/api.php
+++ client/res/api.php
@@ -6,7 +6,27 @@
  *
  */
 
-require_once $_SERVER['DOCUMENT_ROOT'].'/res/mysql.class.php';
+function doQuery($query) {
+	$dbc = new mysqli('localhost','root','root','dfp');
+
+	if (mysqli_connect_errno()) {
+		printf("Connect failed: %s\n", mysqli_connect_error());
+		exit();
+	}
+
+	$result = $dbc->query($query) or die($dbc->error.__LINE__);
+
+	// GOING THROUGH THE DATA
+	if($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+			$all_rows[] = $row;
+		}
+	} else {
+		$all_rows = false;
+	}
+	$dbc->close();
+	return $all_rows;
+}
 
 function createVendorFromShortCode($shortCode) {
 /* Input: shortcode
@@ -14,17 +34,30 @@ function createVendorFromShortCode($shortCode) {
  *
 	*/
 
-//	if(preg_match('/[^a-zA-B0-9]/i', $shortCode))
-//	{
-//		return [
-//			'error' => 'Not a valid Short Code.'
-//		];
-//	}
+	if(preg_match('/[^a-z0-9]/i', $shortCode)) {
+		return [
+			'error' => 'Not a valid Short Code.'
+		];
+	} else {
 
-	$dbc = new mysqli('localhost','root','root','dfp');
-	$result = $dbc->query('SELECT * from vendor;');
-	$foo = mysql_fetch_array($result);
-	print_r($foo);
+		$query = 'SELECT * FROM vendor where shortcode = "'.$shortCode.'"';
+		if ($results = doQuery($query)) {
+			//there were rows
+			return [
+				'error' => 'That short code has been taken.'
+			];
+
+		} else {
+			//there were no rows
+			$query = 'INSERT INTO vendor (shortcode) VALUES ("'.$shortCode.'")';
+			doQuery($query);
+			$results =	['success' => 'Vendor created for shortcode: '.$shortCode];
+		}
+
+		return $results;
+
+	}
+
 
 }
 
@@ -34,6 +67,33 @@ function getVendorInfoFromShortCode($shortCode) {
  *
 	*/
 
+	$query = 'SELECT ven.*,max(vis.pos) as `last_ticket`,
+		ifnull((select max(pos) from visitor where
+		vendor_id = ven.id and served = 1),0)+1 as `now_serving`
+		FROM vendor ven	JOIN visitor vis ON (ven.id = vis.vendor_id)
+		where shortcode = "'.$shortCode.'"';
+	if ($results = doQuery($query)) {
+		//there were rows
+		$thisVenue = $results[0];
+		$query = 'SELECT * FROM visitor where vendor_id = "'.$thisVenue['id'].'"';
+		$results = doQuery($query);
+			if ($results) {
+				$visitorQueue = $results;
+			} else {
+				$visitorQueue = array();
+			}
+		$thisVenue['visitorQueue'] = $visitorQueue;
+		return $thisVenue;
+
+
+	} else {
+		//there were no rows
+		return [
+			'error' => 'No vendor found for that shortcode.'
+		];
+	}
+
+
 }
 
 function markVisitorServed($shortCode,$pos) {
@@ -41,6 +101,21 @@ function markVisitorServed($shortCode,$pos) {
  * Actions: mark the visitor served; send SMS to nums 2,1, and 0 away
  * Output: success/failure
 	*/
+
+	$query = 'SELECT id FROM vendor where shortcode = "'.$shortCode.'"';
+	if ($results = doQuery($query)) {
+		//there were rows
+		$query = 'UPDATE visitor SET served = "1" where pos = "'.$pos.'" AND vendor_id = "'.$results[0]['id'].'"';
+		doQuery($query);
+		return ['success' => 'success'];
+
+	} else {
+		//there were no rows
+		return [
+			'error' => 'No vendor found for that shortcode.'
+		];
+	}
+
 
 
 }
@@ -52,6 +127,28 @@ function createVisitorFromShortCode($shortCode,$ph_num) {
  *      if ph_num no exists, or is served, add to list, and reply new pos
  *
 	*/
+
+	$query = 'SELECT ven.*,max(vis.pos) as `last_ticket`,
+		ifnull((select max(pos) from visitor where
+		vendor_id = ven.id and served = 1),0)+1 as `now_serving`
+		FROM vendor ven	JOIN visitor vis ON (ven.id = vis.vendor_id)
+		where shortcode = "'.$shortCode.'"';
+	if ($results = doQuery($query)) {
+		//there were rows
+		$query = 'UPDATE visitor SET served = "1" where pos = "'.$pos.'" AND vendor_id = "'.$results[0]['id'].'"';
+		doQuery($query);
+		return ['success' => 'success'];
+
+	} else {
+		//there were no rows
+		return [
+			'error' => 'No vendor found for that shortcode.'
+		];
+	}
+
+
+
+
 }
 
 
@@ -65,4 +162,11 @@ function sendSMStoUser($ph_num,$msg) {
 
 }
 
-createVendorFromShortCode('happy');
+$foo = markVisitorServed('happy',2);
+
+echo "<pre>";
+print_r($foo);
+echo "\n\n";
+//echo json_encode($foo);
+echo "</pre>";
+
